b0ba45f4c3bdeff38de9459b5c2bdc7fc8262705
Ajay Tirumala <tsajay@gmail.com>
2017-04-09 16:53:05 -0700

    Lenet Architecture change. Much smoother drive. Accumulated bias over several left steers though.

--- model.py
+++ model.py
@@ -60,14 +60,23 @@ y_train = np.array(measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-model.add(Flatten(input_shape=(160, 320, 3)))
+# model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
-
+exit()
