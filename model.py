8c17d41316dd3c38d11d6deaefd606e5f5ce336d
Ajay Tirumala <tsajay@gmail.com>
2017-04-09 16:12:36 -0700

    Rename train.py to model.py

---
+++ model.py
@@ -0,0 +1,72 @@
+import argparse
+import base64
+from datetime import datetime
+import os
+import shutil
+
+import numpy as np
+import socketio
+import eventlet
+import eventlet.wsgi
+from PIL import Image
+from flask import Flask
+from io import BytesIO
+import csv
+import cv2
+from keras.models import load_model
+import h5py
+from keras import __version__ as keras_version
+
+parser = argparse.ArgumentParser(description='Train a model in Keras.')
+parser.add_argument('-c', '--csv_file', required=True, help='Path to the csv file with training data and path to images.')
+parser.add_argument('-i', '--img_dir', required=True, help='Path to the images directory.')
+
+args = parser.parse_args()
+
+if (not os.path.isfile(args.csv_file)):
+    print ("Unable to find %s, Require a valid CSV file with training data as input." %args.csv_file)
+    exit(1)
+
+if (not os.path.isdir(args.img_dir)):
+    print ("Require a valid directory with images for training the input.")
+    exit(1)
+
+
+lines = []
+with  open(args.csv_file, "r") as csv_file:
+    reader = csv.reader(csv_file)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []    
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    img_path = args.img_dir + '/' + filename
+    if (not os.path.isfile(img_path)):
+        print ("Unable to find the training image %s in the img-dir %s. Please check your args" %(filename, args.img_dir))
+        exit(1)
+    image = cv2.imread(img_path)
+
+    images.append(image)
+    # Using only the center image 
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+
+model.save('model.h5')
+
